4a3832563c3beeb054081a1f6eaba101517774bb
Christopher Enokela <christopherenok@gmail.com>
2023-12-30 15:39:11 +0100

    minor fix

--- index.js
+++ index.js
@@ -1,193 +1,185 @@
-const minuteInput = document.getElementById('minuteInput');
-const secondInput = document.getElementById('secondInput');
-const startButton = document.getElementById('startButton');
+const minuteInput = document.getElementById("minuteInput");
+const secondInput = document.getElementById("secondInput");
+const startButton = document.getElementById("startButton");
 const body = document.body;
 const statusBarImg1 = document.getElementById("status-bar--img1");
 const statusBarImg2 = document.getElementById("status-bar--img2");
 let intervalValue = null;
-let remainingSeconds = 0
+let remainingSeconds = 0;
 const listElement = document.getElementById("menu");
 const pauseIcon = document.getElementById("pause");
 
-
 const toggleList = () => {
-
-    if (listElement.style.display === "block") {
-        listElement.style.display = "none";
-    }
-    else {
-        listElement.style.display = "block";
-    }
-}
-
-function hideMenuList() {
+  if (listElement.style.display === "block") {
     listElement.style.display = "none";
+  } else {
+    listElement.style.display = "block";
+  }
 };
 
-document.body.addEventListener('click', function (event) {
+function hideMenuList() {
+  listElement.style.display = "none";
+}
 
-    if (!event.target.closest('.menu') && !event.target.closest('.menu-list')) {
-        hideMenuList();
-    }
+document.body.addEventListener("click", function (event) {
+  if (!event.target.closest(".menu") && !event.target.closest(".menu-list")) {
+    hideMenuList();
+  }
 });
 
 function toggleMode() {
+  body.classList.toggle("dark_mode");
 
-    body.classList.toggle('dark_mode');
+  if (body.classList.contains("dark_mode")) {
+    listElement.innerHTML = "Switch to light mode";
+    localStorage.setItem("currentMode", "darkMode");
+  } else {
+    listElement.innerHTML = "Switch to dark mode";
+    localStorage.setItem("currentMode", "lightMode");
+  }
 
-    if (body.classList.contains('dark_mode')) {
-        listElement.innerHTML = 'Switch to light mode';
-        localStorage.setItem('currentMode', 'darkMode');
-    }
-    else {
-        listElement.innerHTML = 'Switch to dark mode';
-        localStorage.setItem('currentMode', 'lightMode');
-    }
+  if (statusBarImg1.style.display === "none") {
+    statusBarImg1.style.display = "block";
+    statusBarImg2.style.display = "none";
+  } else {
+    statusBarImg1.style.display = "none";
+    statusBarImg2.style.display = "block";
+  }
 
-    if (statusBarImg1.style.display === "none") {
-        statusBarImg1.style.display = "block";
-        statusBarImg2.style.display = "none";
-    }
-    else {
-        statusBarImg1.style.display = "none";
-        statusBarImg2.style.display = "block";
-    }
-
-    hideMenuList();
+  hideMenuList();
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const storedMode = localStorage.getItem('currentMode');
+document.addEventListener("DOMContentLoaded", function () {
+  const storedMode = localStorage.getItem("currentMode");
 
-    if (storedMode === 'darkMode') {
-        body.classList.toggle('dark_mode');
-        listElement.innerHTML = 'Switch to light mode';
-        statusBarImg1.style.display = 'none';
-        statusBarImg2.style.display = 'block';
-    } else {
-        listElement.innerHTML = 'Switch to dark mode';
-        statusBarImg1.style.display = 'block';
-        statusBarImg2.style.display = 'none';
-    }
+  if (storedMode === "darkMode") {
+    body.classList.toggle("dark_mode");
+    listElement.innerHTML = "Switch to light mode";
+    statusBarImg1.style.display = "none";
+    statusBarImg2.style.display = "block";
+  } else {
+    listElement.innerHTML = "Switch to dark mode";
+    statusBarImg1.style.display = "block";
+    statusBarImg2.style.display = "none";
+  }
 });
 
 function toggleStart() {
+  toggleDelete();
 
-    toggleDelete();
+  totalTimer();
 
-    totalTimer();
-
-    playTimer();
+  playTimer();
 }
 
 const totalTimer = () => {
+  const minutes = parseInt(minuteInput.value) || 0;
+  const seconds = parseInt(secondInput.value) || 0;
 
-    const minutes = parseInt(minuteInput.value) || 0;
-    const seconds = parseInt(secondInput.value) || 0;
+  let totalSeconds = minutes * 60 + seconds;
 
-    let totalSeconds = minutes * 60 + seconds;
+  remainingSeconds = totalSeconds;
 
-    remainingSeconds = totalSeconds;
-
-    updateTimer(totalSeconds);
-}
-
-const updateTimer = (seconds = 0) => {
-
-    const timeTextElement = document.getElementById("time-text");
-
-    timeTextElement.innerHTML = `${Math.floor(seconds / 60)}:${seconds % 60}`;
+  updateTimer(totalSeconds);
 };
 
+const updateTimer = (seconds = 0) => {
+  const timeTextElement = document.getElementById("time-text");
+
+  timeTextElement.innerHTML = `${Math.floor(seconds / 60)}:${seconds % 60}`;
+};
 
 const toggleDelete = () => {
+  const timerStart = document.getElementById("timer--start");
+  const timerCircle = document.getElementById("timer-count");
 
-    const timerStart = document.getElementById("timer--start");
-    const timerCircle = document.getElementById("timer-count");
+  if (timerStart.style.display === "none") {
+    timerStart.style.display = "block";
+    timerCircle.style.display = "none";
+  } else {
+    timerStart.style.display = "none";
+    timerCircle.style.display = "block";
+  }
 
-    if (timerStart.style.display === "none") {
-        timerStart.style.display = "block";
-        timerCircle.style.display = "none";
-    }
-    else {
-        timerStart.style.display = "none";
-        timerCircle.style.display = "block";
-    }
-
-    clearInterval(intervalValue);
-
-}
+  clearInterval(intervalValue);
+};
 
 const digitInput = (event, inputElement) => {
-    const allowedKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Backspace', 'ArrowLeft', 'ArrowRight'];
-    const exceptionKeys = ["Backspace", "ArrowLeft", "ArrowRight"];
+  const allowedKeys = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "Backspace",
+    "ArrowLeft",
+    "ArrowRight",
+  ];
+  const exceptionKeys = ["Backspace", "ArrowLeft", "ArrowRight"];
 
-    if (!allowedKeys.includes(event.key) || (!exceptionKeys.includes(event.key) && inputElement.value.length === 2)) {
-        event.preventDefault();
-    }
-    if (minuteInput.value.trim() !== '' || secondInput.value.trim() !== '') {
-        startButton.removeAttribute('disabled');
-    }
-    else {
-        startButton.setAttribute('disabled', 'true');
-    }
-}
+  if (
+    !allowedKeys.includes(event.key) ||
+    (!exceptionKeys.includes(event.key) && inputElement.value.length === 2)
+  ) {
+    event.preventDefault();
+  }
+  if (minuteInput.value.trim() !== "" || secondInput.value.trim() !== "") {
+    startButton.removeAttribute("disabled");
+  } else {
+    startButton.setAttribute("disabled", "true");
+  }
+};
 
 const inputHandler = (inputElement) => {
-    inputElement.addEventListener('keydown', function (event) {
-        digitInput(event, inputElement);
-    });
+  inputElement.addEventListener("keydown", function (event) {
+    digitInput(event, inputElement);
+  });
 
-    inputElement.addEventListener('input', function (event) {
-        digitInput(event, inputElement);
-    });
-}
+  inputElement.addEventListener("input", function (event) {
+    digitInput(event, inputElement);
+  });
+};
 
 inputHandler(minuteInput);
 inputHandler(secondInput);
 
 const playTimer = () => {
+  if (remainingSeconds > 0) {
+    intervalValue = setInterval(() => {
+      remainingSeconds--;
 
-    if (remainingSeconds > 0) {
-        intervalValue = setInterval(() => {
-            remainingSeconds--;
-
-            updateTimer(remainingSeconds);
-            if (remainingSeconds === 0) {
-                pauseTimer();
-            }
-        }, 1000);
-    }
-}
+      updateTimer(remainingSeconds);
+      if (remainingSeconds === 0) {
+        pauseTimer();
+      }
+    }, 1000);
+  }
+};
 
 const pauseTimer = () => {
-    clearInterval(intervalValue);
-    intervalValue = null;
+  clearInterval(intervalValue);
+  intervalValue = null;
 };
 
 function togglePlay() {
-
-
-    if (pauseIcon.src.endsWith('pause-icon.png')) {
-        pauseIcon.src = './image/play-icon.png';
-        pauseTimer();
-    } else {
-        pauseIcon.src = './image/pause-icon.png';
-        playTimer();
-    }
+  if (pauseIcon.src.endsWith("pause-icon.png")) {
+    pauseIcon.src = "./image/play-icon.png";
+    pauseTimer();
+  } else {
+    pauseIcon.src = "./image/pause-icon.png";
+    playTimer();
+  }
 }
 
 function toggleRefresh() {
-    pauseTimer();
+  pauseTimer();
 
-    totalTimer();
-
-    if (pauseIcon.src.endsWith('play-icon.png')) {
-        pauseIcon.src = './image/pause-icon.png';
-        playTimer();
-    }
-    else {
-        pauseIcon.src = './image/pause-icon.png';
-        playTimer();
-    }
+  totalTimer();
+  pauseIcon.src = "./image/pause-icon.png";
+  playTimer();
 }
